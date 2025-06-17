a70e17f0a5d115772b513d007ffde561da88a168
Yaroslav Mospan <mossspan@gmail.com>
2025-06-17 14:48:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+'use stict'
+
+const cousre = 26;
+
+for (let dollars = 10; dollars <= 100; dollars += 10) {
+    let hrn = cousre * dollars;
+    console.log(`${dollars} долларів, це ${hrn} гривнів.`)
+}
