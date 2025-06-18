83ba77f88ace25721f111e793b1eb3522e5006c0
Yaroslav Mospan <mossspan@gmail.com>
2025-06-18 15:42:03 +0200

    const

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 const cousre = 26;
 
 for (let dollars = 10; dollars <= 100; dollars += 10) {
-    let hrn = cousre * dollars;
-    console.log(`${dollars} долларів, це ${hrn} гривнів.`)
+    const hrn = cousre * dollars;
+    console.log(`${dollars} долларів, це ${hrn} гривнів.`);
 }
